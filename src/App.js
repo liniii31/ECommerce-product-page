928b919340366171f52bdf8e3733b0b45a5d2224
Shalini Anand <42630854+liniii31@users.noreply.github.com>
2023-01-18 14:50:04 +0530

    product page

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import {useState} from 'react';
+
+
+function App() {
+  let [quantity,setQuantity] = useState(0);
+  let [pic,setPic] = useState(1);
+  function changeImage(e){
+    updateThumbnails();
+    e.target.style.opacity="0.5";
+    e.target.style.border="4px solid orange";
+    if(e.target.id==="one"){ 
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-1.jpg') +")";
+      setPic(1);     
+    }else if(e.target.id==="two"){
+      document.getElementById('product').style.backgroundImage = "url(" + require('./image-product-2.jpg') +")";
+      setPic(2); 
+    }else if(e.target.id==="three"){
+      document.getElementById('product').style.backgroundImage = "url(" + require('./image-product-3.jpg') +")"; 
+      setPic(3);
+    }else if(e.target.id==="four"){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-4.jpg') +")"; 
+      setPic(4);
+    }
+  }
+  function updateThumbnails(){
+    Array.from(document.getElementsByClassName('thumbnail')).forEach(ele=>{
+      ele.style.opacity="1";
+      ele.style.border="none";
+    })
+  }
+  function plus(e){
+    if(quantity===10){
+      setQuantity(10);
+    }else{
+      setQuantity(quantity+1);
+    }
+    
+  }
+  function minus(e){
+    if(quantity===0){
+      setQuantity(0);
+    }else{
+      setQuantity(quantity-1);
+    }
+    
+  }
+  function left(e){
+    if(pic===1){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-4.jpg') +")";
+      setPic(4);
+    }else if(pic===2){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-1.jpg') +")";
+      setPic(1);
+    }else if(pic===3){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-2.jpg') +")";
+      setPic(2);
+    }else if(pic===4){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-3.jpg') +")";
+      setPic(3);
+    }
+  }
+  function right(e){
+    if(pic===1){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-2.jpg') +")";
+      setPic(2);
+    }else if(pic===2){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-3.jpg') +")";
+      setPic(3);
+    }else if(pic===3){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-4.jpg') +")";
+      setPic(4);
+    }else if(pic===4){
+      document.getElementById('product').style.backgroundImage= "url(" + require('./image-product-1.jpg') +")";
+      setPic(1);
+    }
+  }
+  return (
+    <div className="App">
+      <Navbar quantity={quantity}/>
+      <main>
+        <div className="main-left">
+          <button id="product" className="product-image"></button>
+          <button id="move-left" onClick={e=>left(e)}><i className="bi bi-chevron-left"></i></button>
+          <button id="move-right" onClick={e=>right(e)}><i className="bi bi-chevron-right"></i></button>
+          <div className="thumbnails">
+            <button id="one" className="thumbnail-1 thumbnail" onClick={e=>changeImage(e)}></button>
+            <button id="two" className="thumbnail-2 thumbnail" onClick={e=>changeImage(e)}></button>
+            <button id="three" className="thumbnail-3 thumbnail" onClick={e=>changeImage(e)}></button>
+            <button id="four" className="thumbnail-4 thumbnail" onClick={e=>changeImage(e)}></button>
+          </div>
+        </div>
+        <div className="main-right">
+          <div className="product-heading">SNEAKER COMPANY</div>
+          <div className="product-heading-2">Fall Limited Edition <br/>Sneaker</div>
+          <div className="product-para">These low profile sneakers are you perfect casual wear <br/> companion. Featuring a durable rubber outer-sole, they'll <br/> withstand everything the weather can offer.</div>
+          <div className="product-price">
+            <div className="price-1">
+              <div>$127.00</div>
+              <div>50%</div>
+            </div>
+            <div className="price-2"><s>$250.00</s></div>
+          </div>
+          <div className="product-buttons">
+            <div className="quantity">
+              <button id="quantity-plus" onClick={e=>plus(e)}><b><i className="bi bi-plus"></i></b></button>
+              <div>{quantity}</div>
+              <button id="quantity-minus" onClick={e=>minus(e)}><b><i className="bi bi-dash"></i></b></button>
+            </div>
+            <button id="buy">Buy Now</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,106 @@
+import './Navbar.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function Navbar(props) {
+    function navbar(e) {
+        if (e.target.checked) {
+            document.getElementById('mobile-navbar').style.display = "block";
+        } else {
+            document.getElementById('mobile-navbar').style.display = "none";
+        }
+    }
+    function close(e) {
+        document.getElementById('mob-nav').checked = false;
+        document.getElementById('mobile-navbar').style.display = "none";
+    }
+    function cart(e) {
+        if (e.target.checked) {
+            document.getElementsByClassName('cart-popup')[0].style.display = "block";
+        } else {
+            document.getElementsByClassName('cart-popup')[0].style.display = "none";
+        }
+    }
+    return (
+        <>
+            <div className="header">
+                <div className="res-nav">
+                    <input type="checkbox" id="mob-nav" onChange={e => navbar(e)} />
+                    <label htmlFor="mob-nav" id="mobile-nav-click"><i className="bi bi-list"></i></label>
+                    <div className="main-heading">sneakers</div>
+                </div>
+                <div className="nav-items">
+                    <ul>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contacts</li>
+                    </ul>
+                </div>
+                <div className="cart">
+                    <input type="checkbox" id="cart-check" onChange={e => cart(e)} />
+                    <label htmlFor="cart-check"><i className="bi bi-cart3 cart-pointer"></i></label>
+                    {props.quantity!==0?(
+                        <>
+                            <span className='quantity-show'>{props.quantity}</span>
+                        </>
+                    ):(
+                        null
+                    )}
+                </div>
+                <div className="dp-div">
+                    <div className="dp">
+
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-navbar">
+                <div className="close-nav">
+                    <button id="close-navbtn" onClick={e => close(e)}><i className="bi bi-x-lg"></i></button>
+                    <div className="mobile-nav-item">
+                        <ul>
+                            <li>Collections</li>
+                            <li>Men</li>
+                            <li>Women</li>
+                            <li>About</li>
+                            <li>Contacts</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="cart-popup">
+                {props.quantity === 0 ? (
+                    <>
+                        <div className="cart-heading">
+                            Cart
+                        </div>
+                        <div className="cart-details-zero">
+                            Nothing in the cart!!
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="cart-heading">
+                            Cart
+                        </div>
+                        <div className="cart-details">
+                            <div className="cart-details-1"></div>
+                            <div className="cart-details-2">
+                                <div className="cart-details-2-1">Fall Limited Edition sneakers</div>
+                                <div className="cart-details-2-2">$127.00x{props.quantity}&nbsp;<b className="black">${127.00*props.quantity}</b></div>
+                            </div>
+                            <div className="cart-details-3">
+                                <button className="delete"><i className="bi bi-trash-fill"></i></button>
+                            </div>
+                        </div>
+                        <div className="cart-button">
+                            <button>Checkout</button>
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
